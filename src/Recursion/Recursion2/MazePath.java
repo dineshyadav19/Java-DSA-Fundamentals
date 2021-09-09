9f4a9b04f773c1de54ca9f5ef4ede798326b8a7c
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2021-09-09 11:03:58 +0530

    Maze path

---
+++ src/Recursion/Recursion2/MazePath.java
@@ -0,0 +1,28 @@
+package Recursion.Recursion2;
+
+public class MazePath {
+    public static void main(String[] args) {
+        mazePath(0, 0, 2, 2, "" );
+    }
+
+    /*
+    * cr ---> current row
+    * cc ---> current column
+    * er ---> end row
+    * ec ---> end column
+    */
+    public static void mazePath(int cr, int cc, int er, int ec, String ans) {
+        //+ base case
+        if(cr == er && cc == ec){
+            System.out.println(ans);
+            return;
+        }
+
+        //- base case
+        if(cr > er || cc > ec)
+            return;
+
+        mazePath(cr + 1, cc, er, ec, ans + 'H');
+        mazePath(cr, cc + 1, er, ec, ans + 'V');
+    }
+}
