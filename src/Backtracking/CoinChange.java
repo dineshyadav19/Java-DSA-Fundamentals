31ca2a8b2d10f569a6a26818e517394885b43a90
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2021-09-15 13:08:28 +0530

    Coin denomination

---
+++ src/Backtracking/CoinChange.java
@@ -0,0 +1,22 @@
+package Backtracking;
+
+public class CoinChange {
+    public static void main(String[] args) {
+        coinChange(new int[] {2, 3, 5, 6}, 10, 0, " ");
+    }
+
+    public static void coinChange(int[] den, int amount, int lastIndex, String ans) {
+        //base case
+        if(amount == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        //self work
+        for (int i = lastIndex; i < den.length; i++) {
+            if(amount >= den[i]) {
+                coinChange(den, amount - den[i], i, ans + den[i] + " ");
+            }
+        }
+    }
+}
