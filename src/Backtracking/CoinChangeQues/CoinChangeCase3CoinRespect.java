c166ff4613a883c5642b9423b69d0df16e6b3b6d
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2021-09-16 08:05:03 +0530

    coin change case 3

---
+++ src/Backtracking/CoinChangeQues/CoinChangeCase3CoinRespect.java
@@ -0,0 +1,31 @@
+package Backtracking.CoinChangeQues;
+
+public class CoinChangeCase3CoinRespect {
+    public static void main(String[] args) {
+        coinChangeCoinRespect(new int[]{2,2,2,3,4 }, 0, 6, " ", true);
+    }
+
+    //Case 1: Coins with Finite Supply ---> Coins can be used only once
+    public static void coinChangeCoinRespect(int[] den, int vidx, int amount, String ans, boolean flag) {
+        //base case
+        if(amount == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        //- base case
+        if(vidx == den.length || amount < 0)
+            return;
+
+        //work
+
+        if(flag == false && den[vidx] == den[vidx - 1]){
+            coinChangeCoinRespect(den, vidx + 1, amount, ans, false);
+        } else {
+            //Included
+            coinChangeCoinRespect(den, vidx + 1, amount - den[vidx], ans + den[vidx], true);
+            //Excluded
+            coinChangeCoinRespect(den, vidx + 1, amount, ans, false);
+        }
+    }
+}
